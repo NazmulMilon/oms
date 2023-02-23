60fee70ecc8291517adba88f2081b2e25255324c
nazmulmilon <milonsarker73@gmail.com>
2023-02-23 08:26:34 +0600

    APP >> Tasks MOD <Tasks> serializers TaskRetrieveSerializer updated

--- online_medicine_shop/tasks/serilizers.py
+++ online_medicine_shop/tasks/serilizers.py
@@ -41,6 +41,19 @@ class TaskListSerializer(ModelSerializer):
 
 
 class TaskRetrieveSerializer(ModelSerializer):
+    category = SerializerMethodField()
+    assigned_by = SerializerMethodField()
+    assigned_to = SerializerMethodField()
+
+    def get_category(self, instance):
+        return instance.category.category_name if instance.category else ""
+
+    def get_assigned_by(self, instance):
+        return instance.assigned_by.username if instance.assigned_by else ""
+
+    def get_assigned_to(self, instance):
+        return instance.assigned_to.username if instance.assigned_to else ""
+
     class Meta:
         model = Task
         exclude = ['created_at', 'updated_at']
